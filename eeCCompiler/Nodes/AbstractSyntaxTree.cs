6c69f705827f27a5a448151a742a368f83370fea
Jgfrausing <jfraus14@student.aau.dk>
2016-03-14 13:27:20 +0100

    GoGoGo :100:

--- eeCCompiler/Nodes/AbstractSyntaxTree.cs
+++ eeCCompiler/Nodes/AbstractSyntaxTree.cs
@@ -1,36 +1,7 @@
-using System.Collections.Generic;
-
 namespace eeCCompiler.Nodes
 {
     public abstract class AbstractSyntaxTree
     {
         //public string Tag { get; set; }
-    }
-
-    internal class Constants : AbstractSyntaxTree
-    {
-        public Constants()
-        {
-            ConstantList = new List<Constant>();
-        }
-
-        public Constants(List<Constant> constants)
-        {
-            ConstantList = constants;
-        }
-
-        public List<Constant> ConstantList { get; set; }
-    }
-
-    internal class Constant : AbstractSyntaxTree
-    {
-        public Constant(Identifier identifier, IConstantPart constantPart)
-        {
-            Identifier = identifier;
-            ConstantPart = constantPart;
-        }
-
-        public Identifier Identifier { get; set; }
-        public IConstantPart ConstantPart { get; set; }
     }
 }
