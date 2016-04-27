989a700697753669a83bda610a7d68e7e9bb1fc8
Matklem <mathiasklemmen@gmail.com>
2016-04-27 16:25:48 +0200

    Visitor for Expression check

---
+++ eeCCompiler/Visitors/ExprCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eeCCompiler.Nodes;
+using eeCCompiler.Interfaces;
+
+namespace eeCCompiler.Visitors
+{
+    class ExprCheck : Visitor
+    {
+        public IValue Value { get; set; }
+
+        public override void Visit(ExpressionParen exprParen)
+        {
+            var newChecker = new ExprCheck();
+            exprParen.Expression.Accept(newChecker);
+            this.Value = newChecker.Value;
+        }
+        public override void Visit(ExpressionVal expressionVal)
+        {
+
+        }
+        public override void Visit(ExpressionValOpExpr expressionValOpExpr) { }
+        public override void Visit(ExpressionParenOpExpr expressionParenOpExpr) { }
+        public override void Visit(ExpressionNegate expressionNegate) { }
+        public override void Visit(ExpressionMinus expressionVal) { }
+        public override void Visit(FuncCall expressionVal) { }
+        public override void Visit(ListType listType) { }
+    }
+}
