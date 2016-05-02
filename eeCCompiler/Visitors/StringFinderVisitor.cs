43273fe8d70d64262af35e9094a14ea67aa619cf
Matklem <mathiasklemmen@gmail.com>
2016-05-02 09:23:14 +0200

    StringFinderVisitor :deciduous_tree:

---
+++ eeCCompiler/Visitors/StringFinderVisitor.cs
@@ -0,0 +1,23 @@
+﻿using eeCCompiler.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eeCCompiler.Visitors
+{
+    class StringFinderVisitor : Visitor
+    {
+        public Queue<StringValue> StringQueue { get; set; }
+
+        public StringFinderVisitor()
+        {
+            StringQueue = new Queue<StringValue>();
+        }
+        public override void Visit(StringValue stringValue)
+        {
+            StringQueue.Enqueue(stringValue);
+        }
+    }
+}
